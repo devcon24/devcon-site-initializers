61b6f7143fa7113ae4be0117c810cb2252d09234
Vitaliy Koshelenko <vetal324@gmail.com>
2024-10-26 00:50:24 +0300

    Workshop: Exercise 12 - Customization

---
+++ exercises/exercise-12/src/main/java/com/devcon/site/initializer/service/SiteInitializerService.java
@@ -0,0 +1,9 @@
+package com.devcon.site.initializer.service;
+
+public interface SiteInitializerService {
+
+    void verifyLayoutSetThemeCSSClientExtension(long groupId);
+
+    void verifySiteNavigationMenuIds(long groupId);
+
+}
